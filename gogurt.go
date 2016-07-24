c9de6ad06b3575ae635df76f908455519496beb5
Femi Agbabiaka <oagbabiaka@activecampaign.com>
2016-07-24 13:43:22 -0500

    Added some error handling for invalid command names and parameters, beginnings of creating string map of branch names.

--- gogurt.go
+++ gogurt.go
@@ -5,26 +5,48 @@ import "os"
 import "os/exec"
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Must supply a branch name.")
+		os.Exit(1)
+	}
+
 	command := os.Args[1]
 	branchName := os.Args[2]
-	var cmdOut []byte
-	var err error
-	fmt.Println(command)
 
 	m := map[string]func(string) string{
 		"branch":   branch,
 		"checkout": checkout,
 	}
 
-	response := m[command](branchName)
-	fmt.Println(z)
+	realCommand, commandExists := m[command]
+	if commandExists != true {
+		fmt.Println("The command " + command + " doesn't exist.")
+		os.Exit(1)
+	}
+
+	response := realCommand(branchName)
+
+	fmt.Println(response)
 
 }
 
 func checkout(branchName string) string {
-	return "checkout func " + branchName
+	return captureBranches()
 }
 
 func branch(branchName string) string {
-	return "branch func " + branchName
+	return captureBranches()
+}
+
+func captureBranches() string {
+	var cmdOut []byte
+	var err error
+	cmdName := "git"
+	cmdArgs := []string{"branch"}
+	if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+		fmt.Fprintln(os.Stderr, "There was en error capturing branches: ", err)
+		os.Exit(1)
+	}
+
+	return string(cmdOut)
 }
